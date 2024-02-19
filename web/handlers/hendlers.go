7ab20e715bf69a0fc63669c368c11c8b79bed196
Evgeniy Sedov <sedoff-online@yandex.ru>
2024-02-19 23:41:50 +0700

    fixes routes & handlers in web server

--- web/handlers/hendlers.go
+++ web/handlers/hendlers.go
@@ -1,45 +1,16 @@
 package handlers
 
 import (
-	"github.com/evsedov/GoCalculator/web/storage"
 	"github.com/gofiber/fiber/v2"
 )
 
 type (
-	MainHandler struct {
-		Storage storage.WebExpressionGetter
-	}
+	MainHandler struct{}
 )
 
 func (mh *MainHandler) GetMain(c *fiber.Ctx) error {
 
-	var exp storage.Expression
-	c.BodyParser(exp)
-	mh.Storage.SetExpressions(exp)
-	expressions := mh.Storage.GetExpressions()
-
 	return c.Render("index", fiber.Map{
-		"Title":       "Distributed calculator",
-		"Expressions": expressions,
+		"Title": "Distributed calculator",
 	})
-}
-
-func (mh *MainHandler) GetExpressions(c *fiber.Ctx) error {
-	var expressions []storage.Expression
-
-	agent := fiber.Get("http://127.0.0.1:81/expressions")
-	statusCode, _, errs := agent.Bytes()
-	if len(errs) > 0 {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"errs": errs,
-		})
-	}
-	err := c.BodyParser(expressions)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"err": err,
-		})
-	}
-
-	return c.Status(statusCode).JSON(expressions)
 }
--- web/routes/routes.go
+++ web/routes/routes.go
@@ -2,17 +2,11 @@ package routes
 
 import (
 	"github.com/evsedov/GoCalculator/web/handlers"
-	"github.com/evsedov/GoCalculator/web/storage"
 	"github.com/gofiber/fiber/v2"
 )
 
 func SetupRoutes(app *fiber.App) {
-	mainHandler := &handlers.MainHandler{
-		Storage: &storage.WebExpressionStorage{
-			Expressions: make([]storage.Expression, 0),
-		},
-	}
+	mainHandler := &handlers.MainHandler{}
 
 	app.Get("/", mainHandler.GetMain)
-	app.Get("/expressions", mainHandler.GetExpressions)
 }
