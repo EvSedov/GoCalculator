23fb43fedeb0e54df510a479ca80eb3a0ba6d705
Evgeniy Sedov <sedoff-online@yandex.ru>
2024-04-22 16:20:29 +0700

    adds a tooltip when exiting the app

--- web/static/js/logout.js
+++ web/static/js/logout.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+
   const logout = document.getElementById('logout');
 
   logout.addEventListener('click', (event) => {
